2b218853dcad50e2704aaca9ff03936e14c2682a
vijay300 <gnvijayk@gmail.com>
2021-01-20 12:39:24 +0530

    [Vijay] Added: custom exception handler to handle the method exception and bad requests

---
+++ src/main/java/com/bridzelabz/employeepayrollapp/exceptions/EmployeePayrollExceptionalHandler.java
@@ -0,0 +1,27 @@
+package com.bridzelabz.employeepayrollapp.exceptions;
+
+import com.bridzelabz.employeepayrollapp.dto.ResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class EmployeePayrollExceptionalHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseDTO> handleMethodArgumentNotValidException(
+                                        MethodArgumentNotValidException exception) {
+        List<ObjectError> errorList = exception.getBindingResult().getAllErrors();
+        List<String> errMsg = errorList.stream()
+                                       .map(objectError -> objectError.getDefaultMessage())
+                                       .collect(Collectors.toList());
+        ResponseDTO responseDTO = new ResponseDTO("Exception while processing REST Request", errMsg);
+        return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.BAD_REQUEST);
+    }
+}
