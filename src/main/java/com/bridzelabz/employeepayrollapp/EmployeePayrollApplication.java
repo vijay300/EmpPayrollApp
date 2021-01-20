5b7dc581790b3ddff88bfdeb804a1ced4ea6fa50
vijay300 <gnvijayk@gmail.com>
2021-01-20 11:42:47 +0530

    [Vijay] Refactored: responseDTO class by using lombok library instead of getter and setter

--- src/main/java/com/bridzelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridzelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeePayrollAppApplication {
+public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		SpringApplication.run(EmployeePayrollApplication.class, args);
 	}
 
 }
