57cedd01d7f67344ebfec99b3f2505a71e71b27a
vijay300 <gnvijayk@gmail.com>
2021-01-20 12:01:37 +0530

    [Vijay] Refactored: properites for username and password by setting the database environment

--- src/main/java/com/bridzelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridzelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -12,6 +12,6 @@ public class EmployeePayrollApplication {
 	public static void main(String[] args) {
 		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
 		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
+		log.info("Employee Payroll DB User is {} ", context.getEnvironment().getProperty("spring.datasource.username"));
 	}
-
 }
