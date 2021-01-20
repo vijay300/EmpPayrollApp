f0f7e9abeb4cfdd76d4acf33c6fc99ad4b258c26
vijay300 <gnvijayk@gmail.com>
2021-01-20 11:48:29 +0530

    [Vijay] Refactored: application properties and main class to use slf4j for logging into console

--- src/main/java/com/bridzelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ src/main/java/com/bridzelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,13 +1,17 @@
 package com.bridzelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+		log.info("Employee Payroll App Started in {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
